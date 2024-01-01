143a76b54149f3b5e2e0cb6eade0c56848883832
danyalkrx <126909264+danyalkrx@users.noreply.github.com>
2024-01-01 18:24:19 -0500

    Update data.js

--- greenhouse/data.js
+++ greenhouse/data.js
@@ -10,7 +10,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://en.serreslavoie.com/cdn/shop/files/image_8e674726-2fee-4500-afb0-dc39981e1cf1.jpg?v=1687979310",
+        "Image": "https://i.etsystatic.com/8922242/r/il/fac982/3153053036/il_570xN.3153053036_dei2.jpg",
         "Family Name": "Crassulaceae",
         "Scientific Name": "Echeveria purpusorum",
         "Common Name": "Urbinia",
@@ -18,7 +18,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://en.serreslavoie.com/cdn/shop/files/image_8e674726-2fee-4500-afb0-dc39981e1cf1.jpg?v=1687979310",
+        "Image": "https://i.etsystatic.com/19730079/r/il/38224c/3705768374/il_fullxfull.3705768374_6vg4.jpg",
         "Family Name": "Euphorbiaceae",
         "Scientific Name": "Acalypha wilkesiana ",
         "Common Name": "Jacob's coat",
@@ -26,7 +26,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://en.serreslavoie.com/cdn/shop/files/image_8e674726-2fee-4500-afb0-dc39981e1cf1.jpg?v=1687979310",
+        "Image": "https://revivalseeds.ca/cdn/shop/files/SpilanthesToothachePlant_BullsEye_300x300.heic?v=1697984249",
         "Family Name": "Asteraceae",
         "Scientific Name": "Acmella oleracea",
         "Common Name": "Toothache Plant",
