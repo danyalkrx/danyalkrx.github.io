567071732ec8c42f2624c13e9b4a3ae974ccd642
danyalkrx <126909264+danyalkrx@users.noreply.github.com>
2024-01-02 13:12:56 -0500

    Update data.js

--- greenhouse/data.js
+++ greenhouse/data.js
@@ -34,7 +34,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://www.desert-tropicals.com/Plants/Aizoaceae/big/Acrodon_bellidiflorus.jpg",
         "Family Name": "Aizoaceae",
         "Scientific Name": "Acrodon sp.",
         "Common Name": "",
@@ -42,7 +42,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://i.etsystatic.com/13841971/r/il/5bf8d3/1151565514/il_1080xN.1151565514_vxb3.jpg",
         "Family Name": "Apocynaceae",
         "Scientific Name": "Adenium obesum",
         "Common Name": "Desert Rose",
@@ -50,7 +50,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://newfs.s3.amazonaws.com/taxon-images-1000s1000/Pteridaceae/adiantum-pedatum-le-fbramley.jpg",
         "Family Name": "Pteridaceae",
         "Scientific Name": "Adiantum pedatum",
         "Common Name": "Northern Maidenhair Fern",
@@ -58,7 +58,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://cdn11.bigcommerce.com/s-2f1yka1ndk/images/stencil/1280x1280/products/1127/1508/Adromischus_cooperi__10464.1691691224.JPG?c=1",
         "Family Name": "Crassulaceae",
         "Scientific Name": "Adromischus cooperi",
         "Common Name": "Plover eggs plant",
@@ -66,7 +66,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://i.etsystatic.com/13687258/r/il/bd168b/1845187886/il_570xN.1845187886_a22p.jpg",
         "Family Name": "Crassulaceae",
         "Scientific Name": "Adromischus cristatus",
         "Common Name": "Crinkle Leaf",
@@ -74,7 +74,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Agathis_robusta_kz8.jpg/450px-Agathis_robusta_kz8.jpg",
         "Family Name": "Araucariaceae",
         "Scientific Name": "Agathis robusta",
         "Common Name": "Kauri Pine",
@@ -82,7 +82,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Agave_americana_R01.jpg/800px-Agave_americana_R01.jpg",
         "Family Name": "Asparagaceae",
         "Scientific Name": "Agave sp.",
         "Common Name": "",
@@ -90,7 +90,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://preview.redd.it/need-advice-for-my-albuca-bracteata-pregnant-onion-v0-a9pe1hz60rgb1.jpg?width=3024&format=pjpg&auto=webp&s=3d0bd2f6f62b160aef78394debe2f755846d7489",
         "Family Name": "Asparagaceae",
         "Scientific Name": "Albuca bracteata",
         "Common Name": "Pregnant Onion",
@@ -98,7 +98,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/6/67/Flowers_of_Allamanda_cathartica_in_West_Bengal%2C_India.jpg",
         "Family Name": "Apocynaceae",
         "Scientific Name": "Allamanda cathartica",
         "Common Name": "Yellow Allamanda / Golden Trumpet",
@@ -106,7 +106,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/e/e2/Aloe_Humilis_%286192233878%29.jpg",
         "Family Name": "Aloeaceae",
         "Scientific Name": "Aloe humilis",
         "Common Name": "Spider Aloe",
@@ -114,7 +114,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://i.etsystatic.com/11587994/r/il/c2dd20/1895889337/il_fullxfull.1895889337_b6cq.jpg",
         "Family Name": "Aloeaceae",
         "Scientific Name": "Aloe vera",
         "Common Name": "Aloe Vera",
@@ -122,7 +122,7 @@ export const inputData = [
         "In Inventory": "Yes"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/6/67/Anchusa_officinalis_16-06-2006_19.44.06.JPG",
         "Family Name": "Boraginaceae",
         "Scientific Name": "Anchusa leptophylla",
         "Common Name": "Anchusa",
@@ -130,7 +130,7 @@ export const inputData = [
         "In Inventory": "No"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://www.gardenia.net/wp-content/uploads/2023/05/Angelica-Archangelica-Angelica.webp",
         "Family Name": "Apiaceae",
         "Scientific Name": "Angelica archangelica",
         "Common Name": "Angelica",
@@ -138,7 +138,7 @@ export const inputData = [
         "In Inventory": "No"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://www.gardenia.net/wp-content/uploads/2023/05/Asclepias-Tuberosa-Butterfly-Weed.webp",
         "Family Name": "Asclepiadaceae",
         "Scientific Name": "Asclepias tuberosa",
         "Common Name": "Butterfly Weed",
@@ -146,15 +146,15 @@ export const inputData = [
         "In Inventory": "No"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://indoor-plants.net/wp-content/uploads/2017/10/Asplenium-dimorphum.jpg",
         "Family Name": "Aspuniaceae",
-        "Scientific Name": "Asplenium dimorphium",
+        "Scientific Name": "Asplenium dimorphum",
         "Common Name": "Austral gem fern",
         "In Seed Bank": "",
         "In Inventory": "No"
     },
     {
-        "Image": "https://static.vecteezy.com/system/resources/previews/004/141/669/original/no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-coming-soon-sign-simple-nature-silhouette-in-frame-isolated-illustration-vector.jpg",
+        "Image": "https://upload.wikimedia.org/wikipedia/commons/4/4e/Achillea_millefolium_5Dsr_9042.jpg",
         "Family Name": "Asteraceae",
         "Scientific Name": "Achillea millefolium",
         "Common Name": "Yarrow",
